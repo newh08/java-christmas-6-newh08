e7131744f05b7db91ec9ccf88b1e34a9c68a73c4
newh08 <newh08@gmail.com>
2023-11-11 07:24:08 +0900

    ✨FEAT: 최종 Application 구현.

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,14 @@
 package christmas;
 
+import christmas.controller.ChristmasEventController;
+import christmas.model.service.ChristmasEventService;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ChristmasEventController controller = new ChristmasEventController(new ChristmasEventService(),
+                new InputView(), new OutputView());
+        controller.start();
     }
 }
