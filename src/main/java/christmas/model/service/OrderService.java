c59e6e3e062fbe39942ad390a2a8ae93b3bb20ba
newh08 <newh08@gmail.com>
2023-11-11 04:44:05 +0900

    ♻️REFACTOR: OrderService 주문한 메뉴들 만드는 로직 개선, OrderMenus 생성자 반영 + 반환값을 DTO 로 변경.

--- src/main/java/christmas/model/service/OrderService.java
+++ src/main/java/christmas/model/service/OrderService.java
@@ -1,18 +1,17 @@
 package christmas.model.service;
 
+import christmas.model.domain.dto.OrderedMenusDto;
 import christmas.model.domain.dto.RequestOrderDto;
 import christmas.model.domain.order.OrderedMenus;
 import christmas.model.domain.order.RequestOrder;
+import christmas.model.util.Converter;
 import java.util.List;
 
 public class OrderService {
 
-    public OrderedMenus makeOrderMenus(List<RequestOrder> requestOrderList) {
-        OrderedMenus orderedMenus = new OrderedMenus();
-        for (RequestOrder requestOrder : requestOrderList) {
-            orderedMenus.addRequestOrder(requestOrder);
-        }
-        return orderedMenus;
+    public OrderedMenusDto makeOrderMenusDto(List<RequestOrder> requestOrderList) {
+        OrderedMenus orderedMenus = new OrderedMenus(requestOrderList);
+        return Converter.from(orderedMenus);
     }
 
     public RequestOrder makeRequestOrder(RequestOrderDto requestOrderDto) {
