02abc9541dbe77ad4475a05d030ad2192191bf38
newh08 <newh08@gmail.com>
2023-11-11 07:18:03 +0900

    ♻️Refactor: EventResults 리펙토링, EventBenefits 적용.

--- src/main/java/christmas/model/domain/event/EventResults.java
+++ src/main/java/christmas/model/domain/event/EventResults.java
@@ -4,31 +4,30 @@ import christmas.model.domain.date.Date;
 import christmas.model.domain.order.OrderedMenus;
 
 public class EventResults {
-    private final TotalDiscount totalDiscount;
-    private Gift gift;
+    private final EventBenefits eventBenefits;
     private int totalBenefitAmount;
     private Badge badge;
 
     public EventResults() {
-        this.totalDiscount = new TotalDiscount();
-        gift = new Gift(0);
+        eventBenefits = new EventBenefits(new TotalDiscount(), new Gift(0));
         totalBenefitAmount = 0;
         badge = Badge.NONE;
     }
 
     public void updateEventResult(OrderedMenus orderedMenus, Date date) {
-        totalDiscount.applyDiscount(orderedMenus, date);
-        gift = orderedMenus.makeGift();
-        totalBenefitAmount = totalDiscount.getTotalDiscount() + gift.getGiftPrice();
-        badge = Badge.getBadge(totalBenefitAmount);
+        eventBenefits.applyDiscount(orderedMenus, date);
+        Gift gift = orderedMenus.makeGift();
+        updateTotalBenefitAmount(gift);
+        eventBenefits.updateGift(gift);
+        badge = Badge.getBadge(Math.abs(totalBenefitAmount));
     }
 
-    public TotalDiscount getTotalDiscount() {
-        return totalDiscount;
+    private void updateTotalBenefitAmount(Gift gift) {
+        totalBenefitAmount = eventBenefits.getTotalDiscount() + gift.getGiftPrice();
     }
 
-    public Gift getGift() {
-        return gift;
+    public EventBenefits getEventBenefit() {
+        return eventBenefits;
     }
 
     public int getTotalBenefitAmount() {
