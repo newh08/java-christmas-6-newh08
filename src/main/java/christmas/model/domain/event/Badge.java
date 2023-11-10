74896759ff311360de56898bf375287d86207b12
newh08 <newh08@gmail.com>
2023-11-11 01:19:10 +0900

    ✨FEAT: Badge 추가 구현, 출력 메시지를 위해 이름 저장하며 반환하기 기능 구현.

--- src/main/java/christmas/model/domain/event/Badge.java
+++ src/main/java/christmas/model/domain/event/Badge.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public enum Badge {
-    NONE(0), STAR(5000), TREE(10000), SANTA(20000);
+    NONE(0, "없음"), STAR(5000, "별"), TREE(10000, "트리"), SANTA(20000, "산타");
     private final int minimumBenefitAmount;
+    private final String name;
 
-    Badge(int minimumBenefitAmount) {
+    Badge(int minimumBenefitAmount, String name) {
         this.minimumBenefitAmount = minimumBenefitAmount;
+        this.name = name;
     }
 
     public static Badge getBadge(int totalBenefitAmount) {
@@ -16,5 +18,10 @@ public enum Badge {
                 .filter(badge -> badge.minimumBenefitAmount <= totalBenefitAmount)
                 .max(Comparator.comparingInt(b -> b.minimumBenefitAmount))
                 .orElse(NONE);
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
