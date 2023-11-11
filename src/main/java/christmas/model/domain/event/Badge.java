e6ae4a73e6855e47c7d5c220627c9d27b00ebe26
newh08 <newh08@gmail.com>
2023-11-11 10:50:34 +0900

    ♻️STYLE: 가독성 위해 공백 추가

--- src/main/java/christmas/model/domain/event/Badge.java
+++ src/main/java/christmas/model/domain/event/Badge.java
@@ -4,7 +4,9 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 public enum Badge {
-    NONE(0, "없음"), STAR(5000, "별"), TREE(10000, "트리"), SANTA(20000, "산타");
+    NONE(0, "없음"), STAR(5000, "별"),
+    TREE(10000, "트리"), SANTA(20000, "산타");
+
     private final int minimumBenefitAmount;
     private final String name;
 
