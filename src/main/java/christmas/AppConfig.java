567b12c739af0b5cfc970c62288c49427cd715fa
newh08 <newh08@gmail.com>
2023-11-12 10:50:52 +0900

    ♻️REFACTOR: AppConfig, Service Layer 생성자 변경 반영.

--- src/main/java/christmas/AppConfig.java
+++ src/main/java/christmas/AppConfig.java
@@ -3,6 +3,7 @@ package christmas;
 import camp.nextstep.edu.missionutils.Console;
 import christmas.controller.ChristmasEventController;
 import christmas.model.service.ChristmasEventService;
+import christmas.model.util.Converter;
 import christmas.view.InputView;
 import christmas.view.OutputView;
 
@@ -12,16 +13,20 @@ public class AppConfig {
         return new ChristmasEventController(christmasEventService(), inputView(), outputView());
     }
 
+    private ChristmasEventService christmasEventService() {
+        return new ChristmasEventService(converter());
+    }
+
+    private Converter converter() {
+        return new Converter();
+    }
+
     private InputView inputView() {
         return new InputView();
     }
 
     private OutputView outputView() {
         return new OutputView();
-    }
-
-    private ChristmasEventService christmasEventService() {
-        return new ChristmasEventService();
     }
 
     public void endEvent() {
