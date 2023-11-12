ca7c0a9442addf511d60def400efe0cd28b0a7cc
newh08 <newh08@gmail.com>
2023-11-12 10:50:35 +0900

    ♻️REFACTOR: ChristmasEventController, Service Layer 변경 반영.

--- src/main/java/christmas/controller/ChristmasEventController.java
+++ src/main/java/christmas/controller/ChristmasEventController.java
@@ -3,12 +3,9 @@ package christmas.controller;
 import christmas.model.domain.dto.EventResultsDto;
 import christmas.model.domain.dto.OrderedMenusDto;
 import christmas.model.domain.dto.DateDto;
-import christmas.model.domain.order.RequestOrder;
 import christmas.model.service.ChristmasEventService;
-import christmas.model.util.Converter;
 import christmas.view.InputView;
 import christmas.view.OutputView;
-import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
 
@@ -32,26 +29,18 @@ public class ChristmasEventController {
         outputView.printOrder(orderedMenusDto);
         outputView.printTotalOrderPrice(orderedMenusDto);
 
-        EventResultsDto eventResultsDto = christmasEventService.makeEventResults(orderedMenusDto, dateDto);
+        EventResultsDto eventResultsDto = christmasEventService.makeEventResultsDto(orderedMenusDto, dateDto);
         outputView.printEventResult(eventResultsDto, orderedMenusDto);
     }
 
     private DateDto makeDateDto() {
-        int userInputDate = repeat(inputView::readDate);
-        return Converter.from(userInputDate);
+        int userInputDate = inputView.readDate();
+        return christmasEventService.makeDateDto(userInputDate);
     }
 
     private OrderedMenusDto makeOrderMenusDto() {
         Map<String, Integer> userInputOrders = inputView.readOrders();
-        List<RequestOrder> requestOrders = makeRequestOrders(userInputOrders);
-        return christmasEventService.makeOrderMenusDto(requestOrders);
-    }
-
-    private List<RequestOrder> makeRequestOrders(Map<String, Integer> userInputOrders) {
-        return userInputOrders.entrySet().stream()
-                .map(Converter::from)
-                .map(christmasEventService::makeRequestOrder)
-                .toList();
+        return christmasEventService.makeOrderedMenusDto(userInputOrders);
     }
 
     private <T> T repeat(Supplier<T> something) {
