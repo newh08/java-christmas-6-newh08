c400aa858dc9981a3fdbf0fa2a7c8926e89d860f
newh08 <newh08@gmail.com>
2023-11-13 12:57:52 +0900

    ♻️REFACTOR: EventResults, 총 혜택 금액 변수에서 제거 및 로직 개선.

--- src/main/java/christmas/model/domain/event/EventResults.java
+++ src/main/java/christmas/model/domain/event/EventResults.java
@@ -3,46 +3,33 @@ package christmas.model.domain.event;
 import static christmas.model.domain.event.EventBenefits.makeInitialConditionEventBenefits;
 
 import christmas.model.domain.date.Date;
-import christmas.model.domain.event.benefit.Gift;
 import christmas.model.domain.order.OrderedMenus;
 
 public class EventResults {
     private final EventBenefits eventBenefits;
-    private int totalBenefitAmount;
     private Badge badge;
 
-    public EventResults(EventBenefits eventBenefits, int totalBenefitAmount, Badge badge) {
+    public EventResults(EventBenefits eventBenefits, Badge badge) {
         this.eventBenefits = eventBenefits;
-        this.totalBenefitAmount = totalBenefitAmount;
         this.badge = badge;
     }
 
     public static EventResults makeInitialConditionEventResults() {
-        return new EventResults(makeInitialConditionEventBenefits(), 0, Badge.NONE);
+        return new EventResults(makeInitialConditionEventBenefits(), Badge.NONE);
     }
 
     public void updateEventResult(OrderedMenus orderedMenus, Date date) {
         eventBenefits.applyDiscount(orderedMenus, date);
-        Gift gift = orderedMenus.makeGift();
-        eventBenefits.updateGift(gift);
-        updateTotalBenefitAmount(gift);
+        eventBenefits.updateGift(orderedMenus);
         updateBadge();
     }
 
-    private void updateTotalBenefitAmount(Gift gift) {
-        totalBenefitAmount = eventBenefits.getDiscountBenefit() + gift.getGiftBenefit();
-    }
-
     private void updateBadge() {
-        badge = Badge.getBadge(Math.abs(totalBenefitAmount));
+        badge = Badge.getBadge(Math.abs(eventBenefits.getTotalBenefit()));
     }
 
     public EventBenefits getEventBenefit() {
         return eventBenefits;
-    }
-
-    public int getTotalBenefitAmount() {
-        return totalBenefitAmount;
     }
 
     public Badge getBadge() {
