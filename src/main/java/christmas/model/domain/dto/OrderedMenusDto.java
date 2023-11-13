5af3af53e89913e59aeeb66076c38e8b0eeaa3c4
newh08 <newh08@gmail.com>
2023-11-13 12:39:53 +0900

    ♻️️REFACTOR: OrderedMenusDto, 리스트 일급 컬렉션으로 변경 및 메시지 생성 로직 RequestOrders 로 이동.

--- src/main/java/christmas/model/domain/dto/OrderedMenusDto.java
+++ src/main/java/christmas/model/domain/dto/OrderedMenusDto.java
@@ -1,31 +1,24 @@
 package christmas.model.domain.dto;
 
+import christmas.model.domain.order.RequestOrders;
 import christmas.model.domain.order.TotalOrderPrice;
-import christmas.model.domain.order.RequestOrder;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class OrderedMenusDto {
-    private final List<RequestOrder> requestOrders;
+    private final RequestOrders requestOrders;
     private final TotalOrderPrice totalOrderPrice;
 
-    public OrderedMenusDto(List<RequestOrder> requestOrders, TotalOrderPrice totalOrderPrice) {
+    public OrderedMenusDto(RequestOrders requestOrders, TotalOrderPrice totalOrderPrice) {
         this.requestOrders = requestOrders;
         this.totalOrderPrice = totalOrderPrice;
     }
 
     public String getOrdersMessage() {
-        return requestOrders.stream()
-                .map(RequestOrder::toString)
-                .collect(Collectors.joining("\n"));
+        return requestOrders.makeOrdersMessage();
     }
 
     public int getTotalOrderPrice() {
         return totalOrderPrice.getTotalOrderPrice();
     }
 
-    public List<RequestOrder> getRequestOrders() {
-        return Collections.unmodifiableList(requestOrders);
-    }
+    public RequestOrders getRequestOrders() { return requestOrders; }
 }
