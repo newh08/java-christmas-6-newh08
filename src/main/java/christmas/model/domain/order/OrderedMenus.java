a27c51fe44760f58498799536ee30962cdef8032
newh08 <newh08@gmail.com>
2023-11-13 12:37:16 +0900

    ♻️REFACTOR: OrderedMenus, 리스트 일급 컬렉션(RequestOrders)으로 변경 및 로직 일급 컬렉션으로 이동.

--- src/main/java/christmas/model/domain/order/OrderedMenus.java
+++ src/main/java/christmas/model/domain/order/OrderedMenus.java
@@ -1,53 +1,21 @@
 package christmas.model.domain.order;
 
-import static christmas.model.util.ErrorMessage.OVER_MAX_MENU_MESSAGE;
-import static christmas.model.util.ErrorMessage.WRONG_ORDER_ONLY_BEVERAGE;
-
 import christmas.model.domain.event.benefit.Gift;
 import christmas.model.domain.menu.MenuCategory;
-import christmas.model.util.MyIllegalArgumentException;
-import java.util.Collections;
-import java.util.List;
 
 public class OrderedMenus {
-    private static final int MAX_NUMBER_OF_MENU = 20;
 
-    private final List<RequestOrder> requestOrders;
+    private final RequestOrders requestOrders;
     private final TotalOrderPrice totalOrderPrice;
 
-    public OrderedMenus(List<RequestOrder> requestOrders) {
-        validateRequestOrders(requestOrders);
+    public OrderedMenus(RequestOrders requestOrders) {
         this.requestOrders = requestOrders;
-        totalOrderPrice = new TotalOrderPrice();
+        totalOrderPrice = updatePrice();
         updatePrice();
     }
 
-    private void validateRequestOrders(List<RequestOrder> requestOrders) {
-        validateTotalOrderQuantity(requestOrders);
-        validateMenuContainNonBeverage(requestOrders);
-    }
-
-    private void validateTotalOrderQuantity(List<RequestOrder> requestOrders) {
-        int totalMenuQuantity = requestOrders.stream()
-                .mapToInt(RequestOrder::getQuantity)
-                .sum();
-        if (totalMenuQuantity > MAX_NUMBER_OF_MENU) {
-            throw new MyIllegalArgumentException(OVER_MAX_MENU_MESSAGE);
-        }
-    }
-
-    private void validateMenuContainNonBeverage(List<RequestOrder> requestOrders) {
-        boolean isAllBeverage = requestOrders.stream()
-                .allMatch(requestOrder -> requestOrder.checkCatalog(MenuCategory.BEVERAGE));
-        if (isAllBeverage) {
-            throw new MyIllegalArgumentException(WRONG_ORDER_ONLY_BEVERAGE);
-        }
-    }
-
-    private void updatePrice() {
-        for (RequestOrder requestOrder : requestOrders) {
-            totalOrderPrice.updateTotalOrderPrice(requestOrder);
-        }
+    private TotalOrderPrice updatePrice() {
+        return requestOrders.updatePrice();
     }
 
     public Gift makeGift() {
@@ -55,14 +23,11 @@ public class OrderedMenus {
     }
 
     public int getNumberOfSpecificCatalogOrder(MenuCategory menuCategory) {
-        return requestOrders.stream()
-                .filter(order -> order.checkCatalog(menuCategory))
-                .mapToInt(RequestOrder::getQuantity)
-                .sum();
+        return requestOrders.getNumberOfSpecificCatalogOrder(menuCategory);
     }
 
-    public List<RequestOrder> getRequestOrders() {
-        return Collections.unmodifiableList(requestOrders);
+    public RequestOrders getRequestOrders() {
+        return requestOrders;
     }
 
     public TotalOrderPrice getTotalOrderPrice() {
