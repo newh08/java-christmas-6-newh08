36ac52e0dfcaaa08b55657c339e64f9e02e948ca
newh08 <newh08@gmail.com>
2023-11-14 18:40:07 +0900

    🔥️REFACTOR: EventBenefits, 메서드 삭제. (EventResults, TotalDiscount 구현 적용)
    로직 변경: EventBenefits 업데이트 메서드인 applyDiscount, updateGift 삭제.
    EventBenefits 을 ChristmasDiscountStrategy 에서 직접 생성해 업데이트 메서드 삭제.
    EventBenefitTest 에 반영

--- src/main/java/christmas/model/domain/event/EventBenefits.java
+++ src/main/java/christmas/model/domain/event/EventBenefits.java
@@ -1,19 +1,16 @@
 package christmas.model.domain.event;
 
 import static christmas.model.domain.event.benefit.Gift.makeInitialConditionGift;
-import static christmas.model.domain.event.benefit.TotalDiscount.makeInitialConditionTotalDiscount;
+import static christmas.model.domain.event.benefit.ChristmasTotalDiscount.makeInitialConditionTotalDiscount;
 
-import christmas.model.domain.date.Date;
 import christmas.model.domain.event.benefit.Gift;
 import christmas.model.domain.event.benefit.TotalDiscount;
-import christmas.model.domain.order.RequestOrders;
-import christmas.model.domain.order.TotalOrderPrice;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class EventBenefits {
     private final TotalDiscount totalDiscount;
-    private Gift gift;
+    private final Gift gift;
 
     public EventBenefits(TotalDiscount totalDiscount, Gift gift) {
         this.totalDiscount = totalDiscount;
@@ -22,14 +19,6 @@ public class EventBenefits {
 
     public static EventBenefits makeInitialConditionEventBenefits() {
         return new EventBenefits(makeInitialConditionTotalDiscount(), makeInitialConditionGift());
-    }
-
-    public void applyDiscount(RequestOrders requestOrders, Date date) {
-        totalDiscount.applyDiscount(requestOrders, date);
-    }
-
-    public void updateGift(TotalOrderPrice totalOrderPrice) {
-        this.gift = totalOrderPrice.makeGiftPerTotalOrderPrice();
     }
 
     public String makeBenefitMessage() {
--- src/test/java/christmas/model/domain/event/EventBenefitsTest.java
+++ src/test/java/christmas/model/domain/event/EventBenefitsTest.java
@@ -1,26 +1,27 @@
 package christmas.model.domain.event;
 
-import static christmas.model.domain.event.EventBenefits.makeInitialConditionEventBenefits;
 import static christmas.model.domain.order.RequestOrdersTest.makeRequestOrdersWithTwoMainThreeDessertThreeAppetizer;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import christmas.model.domain.date.Date;
+import christmas.model.domain.event.benefit.ChristmasTotalDiscount;
 import christmas.model.domain.order.RequestOrders;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class EventBenefitsTest {
 
-    @DisplayName("주문 금액 12만원 이하로 EventBenefits 을 만들면 giftMessage 는 없고, 평일에 디저트 3개를 주문하면 혜택메시지로 '평일 할인: -6,069원' 을 가지고 있다.")
+    @DisplayName("주문 금액 12만원 이상 EventBenefits 을 만들면 giftMessage 로 샴페인이 나오고, 평일에 디저트 3개를 주문하면 혜택메시지로 '평일 할인: -6,069원' 을 가지고 있다.")
     @Test
     void test() {
-        EventBenefits eventBenefits = makeInitialConditionEventBenefits();
         RequestOrders requestOrders = makeRequestOrdersWithTwoMainThreeDessertThreeAppetizer();
         Date date = new Date(7);
-        eventBenefits.applyDiscount(requestOrders, date);
+        ChristmasEventStrategy christmasEventStrategy = new ChristmasEventStrategy(new ChristmasTotalDiscount());
+        EventResults eventResults = christmasEventStrategy.updateEventResult(requestOrders, date);
+        EventBenefits eventBenefits = eventResults.getEventBenefit();
 
         String giftMessage = eventBenefits.getGiftMessage();
-        assertThat(giftMessage).isEqualTo("없음");
+        assertThat(giftMessage).isEqualTo("샴페인 1개");
         String benefitMessage = eventBenefits.makeBenefitMessage();
         assertThat(benefitMessage).contains("평일 할인: -6,069원");
     }
