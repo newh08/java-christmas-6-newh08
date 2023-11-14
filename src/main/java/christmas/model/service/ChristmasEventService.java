c7153c3f7c57b5e6ae2c1f3c385ff5998ce5b279
newh08 <newh08@gmail.com>
2023-11-14 18:03:29 +0900

    ♻️REFACTOR: ChristmasEventService, 필드변수, 생성자 및 메서드 로직 변경. (EventResults, TotalDiscount 구현 적용)
    필드변수 변경: EventStrategy 추가
    생성자 변경: 필드변수 추가로 인해 생성자 변경.
    로직 변경: makeEventResultsDto 메서드 OrderedMenus 에서 필드변수인 EventStrategy 를 통해 EventResults 만들어 EventResultsDto 생성.
    EventService 에서 EventStrategy 를 가지고 이벤트 적용하기 위한 변경.

--- src/main/java/christmas/model/service/ChristmasEventService.java
+++ src/main/java/christmas/model/service/ChristmasEventService.java
@@ -5,7 +5,7 @@ import christmas.model.domain.dto.DateDto;
 import christmas.model.domain.dto.EventResultsDto;
 import christmas.model.domain.dto.OrderedMenusDto;
 import christmas.model.domain.event.EventRepository;
-import christmas.model.domain.event.ChristmasEventStrategy;
+import christmas.model.domain.event.EventResults;
 import christmas.model.domain.event.EventStrategy;
 import christmas.model.domain.order.OrderedMenus;
 import christmas.model.domain.order.RequestOrder;
@@ -16,15 +16,16 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class ChristmasEventService {
-    private static final int MINIMUM_EVENT_PRICE = 10000;
 
     private final Converter converter;
     private final EventRepository eventRepository;
+    private final EventStrategy eventStrategy;
     private long runtimeUserId;
 
-    public ChristmasEventService(Converter converter) {
+    public ChristmasEventService(Converter converter, EventRepository eventRepository, EventStrategy eventStrategy) {
         this.converter = converter;
-        this.eventRepository = EventRepository.getEventRepository();
+        this.eventRepository = eventRepository;
+        this.eventStrategy = eventStrategy;
     }
 
     public DateDto makeDateDto(int userInputDate) {
@@ -41,21 +42,15 @@ public class ChristmasEventService {
     }
 
     public EventResultsDto makeEventResultsDto(OrderedMenusDto orderedMenusDto, DateDto dateDto) {
-        EventStrategy christmasEventStrategy = ChristmasEventStrategy.makeInitialConditionEventResults();
-        if (checkTotalOrderPriceOverMinimumEventPrice(orderedMenusDto)) {
-            executeEvent(orderedMenusDto, dateDto, christmasEventStrategy);
-        }
-        runtimeUserId = eventRepository.save(christmasEventStrategy);
-        return converter.from(christmasEventStrategy);
+        EventResults eventResults = executeEvent(orderedMenusDto, dateDto, eventStrategy);
+        runtimeUserId = eventRepository.save(eventResults);
+        return converter.from(eventResults);
     }
 
-    private static boolean checkTotalOrderPriceOverMinimumEventPrice(OrderedMenusDto orderedMenusDto) {
-        return orderedMenusDto.getTotalOrderPrice() >= MINIMUM_EVENT_PRICE;
-    }
-
-    private void executeEvent(OrderedMenusDto orderedMenusDto, DateDto dateDto, EventStrategy eventStrategy) {
+    private EventResults executeEvent(OrderedMenusDto orderedMenusDto, DateDto dateDto, EventStrategy eventStrategy) {
         OrderedMenus orderedMenus = converter.from(orderedMenusDto);
         Date date = converter.from(dateDto);
-        orderedMenus.applyEvent(eventStrategy, date);
+        return orderedMenus.applyEvent(eventStrategy, date);
+
     }
 }
