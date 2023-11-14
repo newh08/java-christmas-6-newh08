71602a510e918d3eca4d08355c01d7b43b1a5d9d
newh08 <newh08@gmail.com>
2023-11-14 18:46:44 +0900

    🔥️️REFACTOR: EventStrategy, 메서드 삭제 (EventResults, TotalDiscount 구현 적용)
    저장 값 변경 기존: getBadge, getEventBenefit 삭제.
    EventStrategy 의 이벤트 결과 데이터를 EventResults 로 이동에 따른 삭제.

--- src/main/java/christmas/model/domain/event/EventStrategy.java
+++ src/main/java/christmas/model/domain/event/EventStrategy.java
@@ -2,12 +2,7 @@ package christmas.model.domain.event;
 
 import christmas.model.domain.date.Date;
 import christmas.model.domain.order.RequestOrders;
-import christmas.model.domain.order.TotalOrderPrice;
 
 public interface EventStrategy {
-    void updateEventResult(RequestOrders requestOrders, TotalOrderPrice totalOrderPrice, Date date);
-
-    Badge getBadge();
-
-    EventBenefits getEventBenefit();
+    EventResults updateEventResult(RequestOrders requestOrders, Date date);
 }
